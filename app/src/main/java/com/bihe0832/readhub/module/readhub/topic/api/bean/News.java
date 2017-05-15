024ed89e6390f762acc05eaa2b7c20deefbeae4e
erichua(华挺) <erichua@webank.com>
2017-05-15 17:36:34 +0800

    use new network framework

---
+++ app/src/main/java/com/bihe0832/readhub/module/readhub/topic/api/bean/News.java
@@ -0,0 +1,8 @@
+package com.bihe0832.readhub.module.readhub.topic.api.bean;
+
+/**
+ * @desc Created by erichua on 15/05/2017.
+ */
+
+public class News {
+}
---
+++ app/src/main/java/com/bihe0832/readhub/network/ApiClient.java
@@ -0,0 +1,8 @@
+package com.bihe0832.readhub.network;
+
+/**
+ * @desc Created by erichua on 15/05/2017.
+ */
+
+public class ApiClient {
+}
