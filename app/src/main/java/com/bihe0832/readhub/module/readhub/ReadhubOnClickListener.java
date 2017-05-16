d437b69db302f1259bba9660abea70d7f8882b1d
子勰 <code@bihe0832.com>
2017-05-16 09:06:23 +0800

    bug 修复，点击新闻会有toast

--- app/src/main/java/com/bihe0832/readhub/module/readhub/ReadhubOnClickListener.java
+++ app/src/main/java/com/bihe0832/readhub/module/readhub/ReadhubOnClickListener.java
@@ -2,12 +2,9 @@ package com.bihe0832.readhub.module.readhub;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bihe0832.readhub.framework.activity.MainActivity;
 import com.bihe0832.readhub.libware.util.TextUtils;
-import com.bihe0832.readhub.module.readhub.news.request.NewsInfo;
 
 /**
  * Created by zixie on 2017/5/13.
@@ -36,7 +33,6 @@ public class ReadhubOnClickListener implements View.OnClickListener,View.OnLongC
     @Override
     public void onClick(View view) {
         if(null == mView){
-            Toast.makeText(view.getContext(),"这是一个跳转",Toast.LENGTH_LONG).show();
             Intent intent = new Intent(view.getContext(), MainActivity.class);
             intent.putExtra(MainActivity.INTENT_EXTRA_KEY_ITEM_URL, jumpUrl);
             intent.putExtra(MainActivity.INTENT_EXTRA_KEY_ITEM_TITLE, shareTitle);
