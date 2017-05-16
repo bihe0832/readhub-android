9995963d22fb47b3a38bd949202bfa1d467c26f3
子勰 <code@bihe0832.com>
2017-05-16 15:30:21 +0800

    自动构建准备

--- build.sh
+++ build.sh
@@ -24,16 +24,16 @@ fi
 cd $localPath/bin && rm -rf  * && cd $localPath
 
 #构建md5
-echo "********build*******"
-chmod +x $localPath/gradlew
-cd $localPath && ./gradlew clean
-cd $localPath && ./gradlew build
-checkResult
+# echo "********build*******"
+# chmod +x $localPath/gradlew
+# cd $localPath && ./gradlew clean
+# cd $localPath && ./gradlew build
+# checkResult
 
 timeinfo=`date +'%Y%m%d'`
 echo "********copy apk *******"
 cp $localPath/app/build/outputs/apk/app-debug.apk $localPath/bin/readhub-debug-${timeinfo}.apk
 cp $localPath/app/build/outputs/apk/app-release-unsigned.apk $localPath/bin/readhub-release-unsigned-${timeinfo}.apk
-$ANDROID_HOME/build-tools/25.0.1/apksigner sign --ks $localPath/debug.keystore --out $localPath/bin/readhub-release-${timeinfo}.apk $localPath/bin/readhub-release-unsigned-${timeinfo}.apk 
+$ANDROID_HOME/build-tools/25.0.2/apksigner sign --ks $localPath/debug.keystore --ks-pass pass:android --out $localPath/bin/readhub-release-${timeinfo}.apk $localPath/bin/readhub-release-unsigned-${timeinfo}.apk 
 checkResult
 cp $localPath/bin/readhub-release-${timeinfo}.apk $localPath/demo/readhub-release-newer.apk
