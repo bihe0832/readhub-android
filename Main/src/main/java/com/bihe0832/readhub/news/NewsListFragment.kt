3afd8becb918346a17cecb65c9871551456a597e
enzowei <enzowei@tencent.com>
2018-02-07 18:04:33 +0800

    优化

--- Main/src/main/java/com/bihe0832/readhub/news/NewsListFragment.kt
+++ Main/src/main/java/com/bihe0832/readhub/news/NewsListFragment.kt
@@ -17,89 +17,73 @@ import com.tencent.jygame.base.subscribe.ui.AutoLoadDecorator
 import kotlinx.android.synthetic.main.fragment_news_list.*
 
 class NewsListFragment : Fragment() {
-	private val viewModel: NewsListViewModel by lazy { ViewModelProviders.of(this).get(NewsListViewModel::class.java) }
+    private val viewModel: NewsListViewModel by lazy { ViewModelProviders.of(this).get(NewsListViewModel::class.java) }
 
-	private val newsListAdapter: NewsListAdapter by lazy { NewsListAdapter() }
+    private val newsListAdapter: NewsListAdapter by lazy { NewsListAdapter() }
 
-	private val autoLoadDecorator by lazy { AutoLoadDecorator(list) }
+    private val autoLoadDecorator by lazy { AutoLoadDecorator(list) }
 
-	private var canLoadMore = false
+    private var canLoadMore = false
 
-	private var lastCursor = System.currentTimeMillis()
+    private var lastCursor = System.currentTimeMillis()
 
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-														savedInstanceState: Bundle?): View? =
-			inflater.inflate(R.layout.fragment_news_list, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_news_list, container, false)
 
-	override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
 
 //    refreshLayout.setOnPullListener(onPullListener)
-		refreshLayout.setOnRefreshListener(onRefreshListener)
+        refreshLayout.setOnRefreshListener(onRefreshListener)
 
-		list.apply {
-			layoutManager = LinearLayoutManager(context)
-			adapter = newsListAdapter
-		}
+        list.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newsListAdapter
+        }
 
 //    autoLoadDecorator.onScrollStateChanged(newsListAdapter.onScrollStateChanged)
-		autoLoadDecorator.onLoadMore(::getMore)
-	}
+        autoLoadDecorator.onLoadMore(::getMore)
+    }
 
-	override fun onActivityCreated(savedInstanceState: Bundle?) {
-		super.onActivityCreated(savedInstanceState)
-		viewModel.newsList.observe(::getLifecycle, { newsList ->
-			Log.d(TAG, "observe:${newsList?.pageSize}")
-			newsList?.let {
-				if (autoLoadDecorator.isLoadingMore) {
-					newsListAdapter.newsList += it.data
-				} else {
-					newsListAdapter.newsList = it.data
-				}
-				autoLoadDecorator.isLoadingMore = false
-				refreshLayout.isRefreshing = false
-				canLoadMore = true
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.newsList.observe(::getLifecycle, { newsList ->
+            Log.d(TAG, "observe:${newsList?.pageSize}")
+            newsList?.let {
+                if (autoLoadDecorator.isLoadingMore) {
+                    newsListAdapter.newsList += it.data
+                } else {
+                    newsListAdapter.newsList = it.data
+                }
+                autoLoadDecorator.isLoadingMore = false
+                refreshLayout.isRefreshing = false
+                canLoadMore = true
 
-				lastCursor = it.data.last().publishDate.getReadhubTimeStamp()
-			}
-		})
+                lastCursor = it.data.last().publishDate.getReadhubTimeStamp()
+            }
+        })
 
-		viewModel.getNewsList(lastCursor)
-	}
+        viewModel.getNewsList(lastCursor)
+    }
 
-//  private val onPullListener = object : OnPullListener {
-//    override fun onPulling(headView: View?) {
-//      val versionInfo = String.format(resources.getString(R.string.bihe0832_common_refresh_sub_title), TimeUtils.getDateCompareResult(lastRefreshTimeStamp))
-//      refreshLayout.setRefreshSubText(versionInfo)
-//    }
-//
-//    override fun onRefreshing(headView: View?) {
-//    }
-//
-//    override fun onCanRefreshing(headView: View?) {
-//    }
-//
-//  }
 
-	private val onRefreshListener = SwipeRefreshLayout.OnRefreshListener {
-		Log.d(TAG, "onRefresh")
-//    if (refreshLayout.isEnabled && !refreshLayout.isRefreshing) {
-		Log.d(TAG, "start refresh")
-		viewModel.getNewsList()
-//    }
-	}
+    private val onRefreshListener = SwipeRefreshLayout.OnRefreshListener {
+        Log.d(TAG, "start refresh")
+        viewModel.getNewsList()
+    }
 
-	private fun getMore() {
-		Log.d(TAG, "getMore")
-		if (canLoadMore) {
-			viewModel.getNewsList(lastCursor)
-		} else {
-			Toast.makeText(context, "滑到底部了，加载更多~", Toast.LENGTH_SHORT).show()
-		}
-	}
+    private fun getMore() {
+        Log.d(TAG, "getMore")
+        if (canLoadMore) {
+            viewModel.getNewsList(lastCursor)
+        } else {
+            Toast.makeText(context, "滑到底部了，加载更多~", Toast.LENGTH_SHORT).show()
+        }
+    }
 
-	companion object {
-		val TAG = "NewsListFragment"
-		@JvmStatic
-		fun newInstance(): NewsListFragment = NewsListFragment()
-	}
+    companion object {
+        val TAG = "NewsListFragment"
+        @JvmStatic
+        fun newInstance(): NewsListFragment = NewsListFragment()
+    }
 }
--- Main/src/main/java/com/bihe0832/readhub/topic/TopicListAdapter.kt
+++ Main/src/main/java/com/bihe0832/readhub/topic/TopicListAdapter.kt
@@ -5,6 +5,7 @@ import android.graphics.PorterDuffColorFilter
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import com.bihe0832.readhub.R
 import com.ottd.base.topic.CommonViewHolder
 import com.ottd.libs.config.Config
@@ -17,48 +18,44 @@ import kotlin.properties.Delegates
 
 
 class TopicListAdapter : RecyclerView.Adapter<CommonViewHolder>() {
+    private val config by lazy { Config.readConfig(TOPIC_VIEW_TYPE_KEY, TOPIC_VIEW_TYPE_LIST) }
+
     var topicList: List<Topic> by Delegates.observable(emptyList()) { prop, old, new ->
         //    autoNotify(old, new) { o, n -> o.id == n.id }
         notifyDataSetChanged()
     }
 
-
     override fun onBindViewHolder(holder: CommonViewHolder, position: Int) {
         holder.bind(topicList[position]) { topic ->
-            if (Config.readConfig(TOPIC_VIEW_TYPE_KEY, TOPIC_VIEW_TYPE_LIST) == TOPIC_VIEW_TYPE_LIST) {
+            if (config == TOPIC_VIEW_TYPE_LIST) {
                 listTopicTitle.text = topic.title
-                listTopicTips.text = when {
-                    topic.newsArray.size > 3 -> "${topic.newsArray.size} 家媒体已报道"
-                    else -> topic.publishDate.getDateCompareResult()
-                }
+                listTopicTips.setTips(topic)
             } else {
                 summaryTopicTitle.text = topic.title
-                summaryTopicTips.text = when {
-                    topic.newsArray.size > 3 -> "${topic.newsArray.size} 家媒体已报道"
-                    else -> topic.publishDate.getDateCompareResult()
-                }
+                summaryTopicTips.setTips(topic)
+
                 //TODO hardy 摘要被折叠，无法打开
                 summaryTopicSummary.text = topic.summary
 
                 val pixelDrawableSize = Math.round(summaryTopicSubTopic.lineHeight * 0.9f)
 
-                val textViewDrawable = context.resources.getDrawable(R.drawable.ic_star_black_24dp).apply {
-                    setBounds(0, 0, pixelDrawableSize, pixelDrawableSize)
-                    colorFilter = PorterDuffColorFilter(context.resources.getColor(R.color.card_title_color), PorterDuff.Mode.SRC_IN)
-                }
                 summaryTopicSubTopic.apply {
-                    setCompoundDrawables(textViewDrawable, null, null, null)
+                    val drawable = context.resources.getDrawable(R.drawable.ic_star_black_24dp).apply {
+                        setBounds(0, 0, pixelDrawableSize, pixelDrawableSize)
+                        colorFilter = PorterDuffColorFilter(context.resources.getColor(R.color.card_title_color), PorterDuff.Mode.SRC_IN)
+                    }
+                    setCompoundDrawables(drawable, null, null, null)
                     setOnClickListener {
                         OttdFramework.getInstance().showWaitting()
                     }
                 }
 
-                val moreAboutDrawable = context.resources.getDrawable(R.drawable.ic_open_in_new_black_24dp).apply {
-                    setBounds(0, 0, pixelDrawableSize, pixelDrawableSize)
-                    colorFilter = PorterDuffColorFilter(context.resources.getColor(R.color.primary_dark), PorterDuff.Mode.SRC_IN)
-                }
                 summaryTopicMoreAboutTopic.apply {
-                    setCompoundDrawables(moreAboutDrawable, null, null, null)
+                    val drawable = context.resources.getDrawable(R.drawable.ic_open_in_new_black_24dp).apply {
+                        setBounds(0, 0, pixelDrawableSize, pixelDrawableSize)
+                        colorFilter = PorterDuffColorFilter(context.resources.getColor(R.color.primary_dark), PorterDuff.Mode.SRC_IN)
+                    }
+                    setCompoundDrawables(drawable, null, null, null)
                     setOnClickListener {
                         OttdFramework.getInstance().showWaitting()
                     }
@@ -70,16 +67,19 @@ class TopicListAdapter : RecyclerView.Adapter<CommonViewHolder>() {
     override fun getItemCount(): Int = topicList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommonViewHolder {
-        val s = Config.readConfig(TOPIC_VIEW_TYPE_KEY, TOPIC_VIEW_TYPE_LIST)
-        val view = if (s == TOPIC_VIEW_TYPE_SUMMARY) {
-            LayoutInflater.from(parent.context).inflate(R.layout.fragment_topic_item_summary, parent, false)
+        val viewId = if (config == TOPIC_VIEW_TYPE_SUMMARY) {
+            R.layout.fragment_topic_item_summary
         } else {
-            LayoutInflater.from(parent.context).inflate(R.layout.fragment_topic_item_list, parent, false)
+            R.layout.fragment_topic_item_list
         }
-        return CommonViewHolder(view)
+        return CommonViewHolder(LayoutInflater.from(parent.context).inflate(viewId, parent, false))
+    }
+
+    private fun TextView.setTips(topic: Topic) {
+        text = when {
+            topic.newsArray.size > 3 -> "${topic.newsArray.size} 家媒体已报道"
+            else -> topic.publishDate.getDateCompareResult()
+        }
     }
 
 }
-
-
-
